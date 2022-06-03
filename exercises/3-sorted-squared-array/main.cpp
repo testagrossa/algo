4aa1c549872ab6d4b3e0992e4b38fe5161588ad8
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-06-04 00:45:05 +0200

    FORMAT

--- exercises/3-sorted-squared-array/main.cpp
+++ exercises/3-sorted-squared-array/main.cpp
@@ -22,4 +22,5 @@ int main()
 
     // Assertions
     assert(outputA == outputB);
+    assert(outputA == expected);
 }
--- exercises/3-sorted-squared-array/solutions/B-sorted-squared-array.cpp
+++ exercises/3-sorted-squared-array/solutions/B-sorted-squared-array.cpp
@@ -7,20 +7,15 @@ using namespace std;
 // O(n) Time | O(n) Space - where n is the lenght of the imput array
 namespace SolutionB
 {
-
     vector<int> sortedSquaredArray(vector<int> array)
     {
         vector<int> sortedSquares(array.size(), 0);
-
         int smallerValueIdx = 0;
         int largerValueIdx = array.size() - 1;
-
         for (int idx = array.size() - 1; idx >= 0; idx--)
         {
-
             int smallerValue = array[smallerValueIdx];
             int largerValue = array[largerValueIdx];
-
             if (abs(smallerValue) > abs(largerValue))
             {
                 sortedSquares[idx] = smallerValue * smallerValue;
@@ -32,7 +27,6 @@ namespace SolutionB
                 largerValueIdx++;
             }
         }
-
         return sortedSquares;
     }
 }
