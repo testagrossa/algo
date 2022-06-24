164249110ebd03a995dddf58199406e54625fd6d
Daguis <daguis128@gmail.com>
2022-06-24 19:44:21 -0300

    FIX main

--- exercises/7-class-photos/main.cpp
+++ exercises/7-class-photos/main.cpp
@@ -12,7 +12,6 @@ int main()
     vector<int> redShirtHeights = { 5, 8, 1, 3, 4 };
     vector<int> blueShirtHeights = { 6, 9, 2, 4, 5 };
     bool expected = true;
-    auto outputA = SolutionA::classPhotos(redShirtHeights, blueShirtHeights);
 
     bool outputA = SolutionA::classPhotos(redShirtHeights, blueShirtHeights);
     std::cout << "outputA: " << std::boolalpha << outputA << std::endl;
